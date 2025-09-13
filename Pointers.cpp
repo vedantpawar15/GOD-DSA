443308907a3aa44a5a9fe4cd2aef564391ca1cdc
vedantpawar15 <ravindrapawar8433@gmail.com>
2025-09-13 10:27:04 +0530

    ptr2

--- Pointers.cpp
+++ Pointers.cpp
@@ -3,12 +3,12 @@ using namespace std;
 
 int main () {
     int arr[3] = {2, 4, 6};
-    int *p = &arr[10];
+    int *p = &arr[1];
 
     cout << arr << endl;
     cout << &arr[0] << endl;
     cout << &arr[1] << endl;
 
     cout << p << endl;
-    cout << *p << endl;
+    cout << *p << endl; 
 }
